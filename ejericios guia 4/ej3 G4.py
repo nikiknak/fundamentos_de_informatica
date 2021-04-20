3f5881c3cc6eaad997afca15d53c4c2b62e8a076
nikiknak <nicoleknak11@gmail.com>
2021-04-20 20:44:23 -0300

    Revert "trabajos practicos"
    This reverts commit f25ba1d84c165cf462189f6d2782c6401a37f2c0.

--- ejericios guia 4/ej3 G4.py
+++
@@ -1,5 +0,0 @@
-#3: Escribí un programa que lea un archivo, guarde las líneas del archivo en una lista y luego imprima las n últimas.
-import re
-f= open("/Users/niki/Desktop/INFORMATICA/UCEMA_Fundamentos_de_informatica-master/Python_intro/manipulacion_archivos.txt","r")
-mensaje =f.readlines()
-print(mensaje[-5:-1])
